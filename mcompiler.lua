b95f657f47fa4e72479f6f51114f336d7f0f773c
Odyssey2247 <bl455.dj@gmail.com>
2019-05-04 00:42:30 -0400

    finish plugin

--- mcompiler.lua
+++ mcompiler.lua
@@ -1,12 +1,37 @@
-MakeCommand("demo", "demo.demoCommand", 0)
+VERSION = "1.0.0"
 
-function demoCommand()
-    CurView():Save(false)
-    runDemo()
+if GetOption("runc") == nil then
+    AddOption("runc", true)
 end
 
-function runDemo()
-    local ft = CurView().Buf:FileType()
-    local file = CurView().Buf.Path
-    local dir = DirectoryName(file)
+if GetOption("build") == nil then
+    AddOption("build", true)
+end
+
+MakeCommand("runc", "mcompiler.runcCommand", 0)
+MakeCommand("build", "mcompiler.buildCommand", 0)
+
+-- example functions to run/compile golang
+--function to run code
+function runcCommand()
+    CurView():Save(false)                 -- save current open file
+    local ft = CurView().Buf:FileType()   -- get file extension  
+    local file = CurView().Buf.Path       -- get file name
+    local dir = DirectoryName(file)       -- get directory of file
+    -- change this
+    local runn = "go run ".. file         -- compiler/interprter params, change this for custom params 
+    os.execute(runn)                      -- ejecute 
+    --os.execute("tmux run-shell -t 2 '" .. runn .. "' " ) -- temux pane (pane 2) output use in t-ide
+end
+
+--function to compile code
+function buildCommand()
+    CurView():Save(false)                 -- save current open file
+    local ft = CurView().Buf:FileType()   -- get file extension  
+    local file = CurView().Buf.Path       -- get file name
+    local dir = DirectoryName(file)       -- get directory of file
+    -- change this
+    local runn = "go build ".. file         -- compiler/interprter params, change this for custom params  
+    os.execute(runn)                      -- ejecute 
+    --os.execute("tmux run-shell -t 2 '" .. runn .. "' " ) -- temux pane (pane 2) output use in t-ide
 end
